a34c051b7f07c2b5f8936336513ea48eb4033405
NickGelfand <102976625+NickGelfand@users.noreply.github.com>
2022-04-14 15:23:18 +0300

    v2

--- chess.js
+++ chess.js
@@ -1,27 +1,25 @@
 let chessBord = document.createElement('table');
 chessBord.className = "styleChess";
-for(let i = 0;i<8;i++)
+for(let i = 0;i<8;i++) 
 {
-    let tr = document.createElement('tr');
+    let tr = document.createElement('tr'); //creates new row
     for(let j=0;j<8;j++)
     {
-        if((j+i)%2==0)
+     //If the sum of cell coordinates is even
+     // then color the cell white
+        if((j+i)%2==0) 
         {
-            let td = document.createElement('td');
-            td.className = "white";
-            console.log("white");
+            let td = document.createElement('td'); //create new content
+            td.className = "white"; //class style attached
             tr.appendChild(td);
         }
         else{
-            let td = document.createElement('td');
-            td.className = "black";
-            console.log("black")
+            let td = document.createElement('td'); //create new content
+            td.className = "black"; //class style attached
             tr.appendChild(td);
         }
     }
-    
-    chessBord.appendChild(tr);
-    console.log("\n");
+    chessBord.appendChild(tr);//add to table the row 
 }
 
-document.body.appendChild(chessBord);
+document.body.appendChild(chessBord);//add the table under the body 
